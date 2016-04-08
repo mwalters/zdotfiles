39889ee4c0381a7a56d509e577d40d8781b4d19d
Matt Walters <me@mattwalters.net>
2016-04-08 09:09:34 -0400

    Changes to apt aliases

--- apt/aliases.zsh
+++ apt/aliases.zsh
@@ -1,5 +1,8 @@
-alias acs="apt-cache search"
-alias aup="sudo apt-get update && sudo apt-get dist-upgrade"
+alias acs="apt search"
+alias aup="sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove"
 alias aclean="sudo apt-get autoremove"
-alias ai="sudo apt-get install"
-alias arm="sudo apt-get remove"
+# alias aup="sudo apt update && sudo apt full-upgrade && aclean"
+alias ai="sudo apt install"
+alias arm="sudo apt remove"
+
+alias makedeb="dpkg --build"
