1c2fd5e43e87a769ab7827bd9218ccb9b583fe3b
Matt Walters <me@mattwalters.net>
2016-04-08 09:09:55 -0400

    Getting some helpful git added

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,12 +4,27 @@ export EDITOR="subl -w"
 # Git aliases
 alias gs='git status -sb'
 alias gll='git log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)[%cn]%Creset' --abbrev-commit --date=relative"
-alias ga='git add .'
-alias gc='git commit -a'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)[%cn]%Creset' --abbrev-commit --date=relative | more"
+alias gbl='git branch -v'
 alias gco='git checkout'
 alias gd='git diff'
+alias gunstage='git reset HEAD'
+
 alias gwtf='$HOME/.dotfiles/git/git-wtf'
 
-# Next one requires `xsel` to be installed`
+# Branching
+alias gbranch-name='git rev-parse --abbrev-ref HEAD'
+alias gpublish='git push -u origin $(git gbranch-name)'
+alias gunpublish='git push origin :$(git gbranch-name)'
+
+# Next one requires `xsel` to be installed
 alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | xsel -ib"
+
+# Mark a file as "assume unchanged", which means that Git will treat it
+# as though there are no changes to it even if there are. Useful for
+# temporary changes to tracked files
+alias gassume='git update-index --assume-unchanged'
+# Reverse the above
+alias gunassume='git update-index --no-assume-unchanged'
+# Show the files that are currently assume-unchanged
+alias gassumed='git ls-files -v | grep ^h | cut -c 3-'
