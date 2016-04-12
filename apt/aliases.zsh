382f21f6d90a18f596942ee9c70cc88235c520a3
Matt Walters <me@mattwalters.net>
2016-04-12 16:03:06 -0400

    Messing with aliases

--- apt/aliases.zsh
+++ apt/aliases.zsh
@@ -1,7 +1,7 @@
 alias acs="apt search"
-alias aup="sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove"
+# alias aup="sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove"
 alias aclean="sudo apt-get autoremove"
-# alias aup="sudo apt update && sudo apt full-upgrade && aclean"
+alias aup="sudo apt update && sudo apt full-upgrade && aclean"
 alias ai="sudo apt install"
 alias arm="sudo apt remove"
 
