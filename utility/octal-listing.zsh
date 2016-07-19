818dc6ba55786f8fcc5091e261dbf45bda76bcc7
Matt Walters <me@mattwalters.net>
2016-07-19 07:48:20 -0400

    Improves loctal alias

--- utility/octal-listing.zsh
+++ utility/octal-listing.zsh
@@ -1,2 +1,2 @@
 # List directory with permissions as octal
-alias loctal="ls -l | sed -e 's/--x/1/g' -e 's/-w-/2/g' -e 's/-wx/3/g' -e 's/r--/4/g'  -e 's/r-x/5/g' -e 's/rw-/6/g' -e 's/rwx/7/g' -e 's/---/0/g'"
+alias loctal="ls -la | sed -e 's/--x/1/g' -e 's/-w-/2/g' -e 's/-wx/3/g' -e 's/r--/4/g'  -e 's/r-x/5/g' -e 's/rw-/6/g' -e 's/rwx/7/g' -e 's/---/0/g'"
