406765b9e81d0882c695fd1d0e1e0e1663aa52e6
Matt Walters <me@mattwalters.net>
2018-10-20 17:07:56 -0400

    Add aliases for dealing with docker containers/images

---
+++ docker/aliases.zsh
@@ -0,0 +1,32 @@
+# Remove dangling images
+alias dkrmdangle="docker images -q --filter dangling=true | xargs docker rmi"
+
+# Prune containers; remove containers that are stopped
+alias dkp="docker container prune -f"
+
+# Get container process
+alias dkps="docker ps"
+
+# Get process included stop container
+alias dkpa="docker ps -a"
+
+# Get images
+alias dki="docker images"
+
+# Stop all containers
+dkstop() { docker stop $(docker ps -a -q); }
+
+# Remove all containers
+dkrm() { docker rm $(docker ps -a -q); }
+
+# Stop and Remove all containers
+alias dkrmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# Remove all images
+dkri() { docker rmi $(docker images -q); }
+
+# Show all alias related docker
+dkalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
+
+# Bash into running container
+dkbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
