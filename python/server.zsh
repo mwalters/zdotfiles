f810a3df91d44a043e00b4cc2903744c399e99e0
Matt Walters <me@mattwalters.net>
2020-10-24 17:01:25 -0400

    Update to python3 static asset server

--- python/server.zsh
+++ python/server.zsh
@@ -1,5 +1,5 @@
 # Start an HTTP server from a directory, optionally specifying the port; defaults to 8080
 function server() {
     local port="${1:-8080}"
-    python -m SimpleHTTPServer $port
+    python3 -m http.server $port
 }
